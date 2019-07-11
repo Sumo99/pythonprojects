ee105c3d8598965110f9f2c1a1288986ed2e70a3
Sumo99 <36107758+Sumo99@users.noreply.github.com>
2019-07-10 23:12:26 -0500

    Update github_issue_alerter.py

--- github_issue_alerter.py
+++ github_issue_alerter.py
@@ -1,4 +1,26 @@
 import requests
-r = requests.get('https://api.github.com/search/issues?q=%22%22+label:%22Good+First+Issue%22+state:open')
-r.json()
-#to do use the notifier subprocess to alert the user when a issue is available!
+import json
+from requests.auth import HTTPBasicAuth
+username = 'sumo99'
+password = 'jR22AtfeR9bNbSe'
+r = requests.get(
+    'https://api.github.com/search/issues?utf8=✓&q=is%3Aopen+is%3Aissue+label%3A"good+first+issue"')
+repo_urls = []
+if(r.ok):
+    repoItem = json.loads(r.text or r.content)
+    resultSet = repoItem.get("items")
+    for result in resultSet:
+        repo_urls.append(result.get("repository_url"))
+resultSet = []
+for apiUrl in repo_urls:
+    print(apiUrl)
+    repo_details = requests.get(apiUrl, auth=HTTPBasicAuth(username, password))
+    if(repo_details.ok):
+        repoResult = json.loads(repo_details.text or repo_details.content)
+        resultSet.append(repoResult.get("full_name"))
+    else:
+        print(repo_details)
+        print("Error getting the repository!")
+for result in resultSet:
+    print(resultSet)
+print(resultSet[0])
